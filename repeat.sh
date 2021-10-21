b24cc942f87ade01227b6d9ddbbc35b49758c8f3
Peter Willis <31324861+peterwwillis@users.noreply.github.com>
2021-10-21 13:00:14 -0400

    Refactor and change some functionality
     - Add a '-q' option to silence output unless a command fails.
    
     - Rather than passing one COMMAND and arguments on the command-line,
       you now pass multiple independent COMMANDs, which will each get
       the '-f' contents passed to them.
    
     - The shell now executes the command as an unquoted expansion of
       the line from '-f' and the COMMAND. This will probably present
       some problems related to whitespaces or special characters...
       Not sure of an easy way to solve this. I'm doing it because
       otherwise we have the COMMAND/arguments passed independent as
       ARGV to the script, and then the '-f' which is a string.
    
     - Generally refactor code to reduce some duplication.
       As a result, the logging function now runs the commands...
       This is probably terrible, but it does make the code more
       compact?
    
     - I'll probably regret all of these changes.

--- repeat.sh
+++ repeat.sh
@@ -8,52 +8,64 @@ set -eu
 sleep_lock_wait="1"
 sleep_after_unlock="1"
 background=0
-out_file='/dev/stdout'
+quiet=0
 read_delim=$'\n'
 read_file=
-logdir=
+log_dir=
+log_file=/dev/stdout
+lock_file=
 
 
 _exittrap () {
     trap - SIGTERM && kill -- -$$
 }
-_info () { echo "$(date) $0 $BASHPID: $*" ; }
+_log () {
+    local output="$1" result="${2:-0}" cmd="${3:-}" logf="$log_file"
+    local log="$(date) $0 $BASHPID: $output"
+    if [ "$quiet" = "1" ] && [ "$result" = "0" ] ; then
+        logf="/dev/null"
+    fi
+    [ -z "$output" ] && log="" # print an empty line if output is empty
+    echo "$log" >> "$logf"
+    if [ -n "$cmd" ] ; then
+        # This is where all commands are actually run!
+        $cmd >> "$logf" 2>&1
+    fi
+}
 _process_lock () {
     local chksum="$1"; shift
-    while [ -e "$logdir/$chksum.lock" ] ; do
-        read -a lock_data < "$logdir/$chksum.lock"
+    while [ -e "$lock_file" ] ; do
+        read -a lock_data < "$lock_file"
         ppid="${lock_data[0]}"
         cmdname="${lock_data[1]}"
         if ! kill -0 "$ppid" 2>/dev/null || [ ! "$cmdname" = "$0" ] ; then
-            _info "ppid '$ppid' gone or cmdname '$cmdname' != '$0'; lock stale, removing '$logdir/$chksum.lock'"
-            rm -f "$logdir/$chksum.lock"
+            _log "ppid '$ppid' gone or cmdname '$cmdname' != '$0'; lock stale, removing '$lock_file'"
+            rm -f "$lock_file"
             continue
         fi
-        _info "Waiting for '$logdir/$chksum.lock'"
+        _log "Waiting for '$lock_file'"
         sleep "$sleep_lock_wait"
     done
 }
 _process_cmd () {
     local result chksum
 
-    if [ -n "$logdir" ] ; then
+    if [ -n "$log_dir" ] ; then
         chksum="$(printf "%s\n" "$0 $*" | md5sum - | awk '{print $1}')"
+        lock_file="$log_dir/$chksum.lock"
+        log_file="$log_dir/$chksum.log"
         _process_lock "$chksum"
-        echo "$BASHPID $0 $*" > "$logdir/$chksum.lock"
-        echo "" >>"$logdir/$chksum.log"
-        _info "running command: $*" >> "$logdir/$chksum.log" 2>&1
-        "$@" >>"$logdir/$chksum.log" 2>&1
-        result=$?
-        echo "" >>"$logdir/$chksum.log"
-        _info "command '$*' exited with status $result" >> "$logdir/$chksum.log" 2>&1
-        rm -f "$logdir/$chksum.lock"
-    else
-        echo ""
-        _info "running command: $*"
-        "$@" 2>&1
-        result=$?
-        echo ""
-        _info "command '$*' exited with status $result"
+        echo "$BASHPID $0 $*" > "$lock_file"
+    fi
+
+    _log ""
+    _log "running command: $*" 0 "$*"
+    result=$?
+    _log ""
+    _log "command '$*' exited with status $result" $result
+
+    if [ -n "$lock_file" ] ; then
+        rm -f "$lock_file"
     fi
 
     sleep "$sleep_after_unlock"
@@ -63,18 +75,31 @@ _process_loop () {
 	while true ; do _process_cmd "$@" ; done
 }
 _process () {
+    declare -a cmds=("$@")
     if [ $background -eq 1 ] ; then
-        out_file='/dev/null'
+        log_file=/dev/null
     fi
     if [ -n "$read_file" ] ; then
         while IFS= read -r -d "$read_delim" arg ; do
             declare -a args=()
-            [ $# -gt 0 ] && args=("$@")
-            args+=("$arg")
-            _process_loop "${args[@]}" >"$out_file" 2>&1 &
+
+            if [ ${#cmds[@]} -gt 0 ] ; then
+                for cmd in "${cmds[@]}" ; do
+                    args=("$cmd")
+                    args+=($arg)
+                    _process_loop "${args[@]}" >"$log_file" 2>&1 &
+                done
+
+            else
+                args+=($arg)
+                _process_loop "${args[@]}" >"$log_file" 2>&1 &
+
+            fi
         done < "$read_file"
+
     else
-        _process_loop "$@" >"$out_file" 2>&1 &
+        _process_loop "$@" >"$log_file" 2>&1 &
+
     fi
     if [ $background -eq 0 ] ; then
         trap _exittrap SIGINT SIGTERM EXIT
@@ -84,29 +109,30 @@ _process () {
 
 _usage () {
     cat <<EOUSAGE
-Usage: $0 [OPTIONS] [COMMAND [ARGS ..]]
+Usage: $0 [OPTIONS] [COMMAND ..]
 
-Continuously runs COMMAND (and any ARGS) in a loop.
+Continuously runs one or more COMMANDs in a loop. Each COMMAND will be run by
+the shell, including any arguments.
 
 If '-l' is passed, a lock file is used to track what commands are still running,
 and STDOUT and STDERR are redirected to a log file. The lock file contains the
-PID, process name, and command and arguments being executed. The log and lock
-file names are the MD5 checksum of the contents of the lock file. The same lock
-and log file names are used for the duration of a repeat.sh run (per command).
+repeat.sh PID, process name, and COMMAND. The log & lock file names are the MD5
+checksum of the lock file, minus the PID. The same log & lock file names are 
+used for the duration of a repeat.sh run (per command).
 
-If '-f' is passed, reads a file and passes each line as a final argument to
+If '-f' is passed, reads a FILE and appends the contents to the end of each
 COMMAND. If '-f' is passed but no COMMAND is passed, executes each line of the
-file as a list  of COMMANDs and ARGS. If '-0' is passed, separates lines by the
-null character.
+file as a list of COMMANDs. If '-0' is passed, lines are separated by nulls.
 
 Options:
   -w SEC        Seconds to sleep after a command completes and the lock is removed. ($sleep_after_unlock)
   -W SEC        Seconds to sleep in between checking for a lock to be released. ($sleep_lock_wait)
   -f FILE       Read commands from FILE. If '-', reads from standard input.
   -l DIR        Output logs and lock files to DIR.
-  -o FILE       Output the loop's warnings/errors to a FILE. ($out_file)
-  -0            If a FILE was passed, separate each entry by the null byte, not newlines
-  -b            Fork the command(s) into the background and exit
+  -o FILE       Output the loop's warnings/errors to a FILE. ($log_file)
+  -0            If a FILE was passed, separate each entry by the null byte, not newlines.
+  -b            Fork the command(s) into the background and exit.
+  -q            Do not log command output or output any output unless a command fails.
   -h            This screen
   -v            Debug mode
 EOUSAGE
@@ -114,7 +140,7 @@ EOUSAGE
 }
 
 
-while getopts "w:W:f:l:o:0bhv" args ; do
+while getopts "w:W:f:l:o:0bqhv" args ; do
     case $args in
         w)  sleep_after_unlock="$OPTARG" ;;
         W)  sleep_lock_wait="$OPTARG" ;;
@@ -122,10 +148,11 @@ while getopts "w:W:f:l:o:0bhv" args ; do
             if [ "$read_file" = "-" ] ; then
                 read_file="/dev/stdin" ; # bash will emulate /dev/stdin
             fi ;;
-        l)  logdir="$OPTARG" ;;
-        o)  out_file="$OPTARG" ;;
+        l)  log_dir="$OPTARG" ;;
+        o)  log_file="$OPTARG" ;;
         0)  read_delim=$'\0' ;;
         b)  background=1 ;;
+        q)  quiet=1 ;;
         h)  _usage ;;
         v)  DEBUG=1 ; set -x ;;
         *)
@@ -139,8 +166,8 @@ if [ -z "$read_file" ] && [ $# -lt 1 ] ; then
     _usage
 fi
 
-if [ -n "$logdir" ] && [ ! -d "$logdir" ] ; then
-    mkdir -p "$logdir"
+if [ -n "$log_dir" ] && [ ! -d "$log_dir" ] ; then
+    mkdir -p "$log_dir"
 fi
 
 _process "$@"
